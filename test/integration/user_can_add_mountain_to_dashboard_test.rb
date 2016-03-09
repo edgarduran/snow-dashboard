770f1be23608b57c4150f0a2a21eb5e088789bf9
Edgar Duran <edgarduran@Edgars-MacBook-Pro-8.local>
2016-03-09 10:01:51 -0700

    tests for search and login

--- test/integration/user_can_add_mountain_to_dashboard_test.rb
+++ test/integration/user_can_add_mountain_to_dashboard_test.rb
@@ -1,5 +1,25 @@
 require 'test_helper'
 
 class UserCanAddMountainToDashboardTest < ActionDispatch::IntegrationTest
-  
+
+  test "user can search for resorts" do
+    login_user
+
+    within('.resort-search') do
+      select 'CO', from: "state"
+      fill_in "city", with: "dillon"
+      fill_in "Resort name", with: "keystone"
+      click_on "Search"
+    end
+
+    assert page.has_content?("Keystone Ski Resort")
+    assert page.has_content?("Colorado")
+    assert page.has_button?("Add to Dashboard")
+  end
+
+  test "user can add a resort to their dashboard" do
+    
+  end
+
+
 end
